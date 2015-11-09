9886ff26f80f8500c6b81ff9b543f0ee7a58aa5f
Ya <wuandy1225@gmail.com>
2015-11-10 00:59:59 +0800

    change question image&add charaMoveBack function
    1.character.lua add charaMoveBack function make hero move back
    2.change question image

--- question1.lua
+++ question1.lua
@@ -260,7 +260,7 @@ end
 
 function questionMark1.new (originPointX,originPointY)
     local obj = {
-        Image = love.graphics.newImage("img/hint.png"),
+        Image = love.graphics.newImage("img/puzzle.png"),
         Barrier=true,
         x = originPointX,
         y = originPointY
