b935d8d24bb32c9d10d622783178eac0008c8919
¡§sexy¡¨ <gt810034@gmail.com>
2015-11-15 01:41:43 +0800

    add starSky to menu

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ function love.load()
     require "loveavg"
     require "setting"
     require "world"
-    gameStage = 0
+    gameStage = 1
     isSet = false
         op_load()
         menu_load()
