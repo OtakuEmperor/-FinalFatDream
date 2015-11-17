a9437422fcca739acbe30c9f359f3fa0a41981d7
¡§sexy¡¨ <gt810034@gmail.com>
2015-11-17 16:51:43 +0800

    add audio to menu and beiCheeKiller

--- beiCheeKiller.lua
+++ beiCheeKiller.lua
@@ -12,59 +12,38 @@ function beiCheeKiller_load()
     --set select
     beiCheeKiller.stage = 1
     beiCheeKiller.isSelect = true
+    --load audio
+    beiCheeKiller.button = love.audio.newSource("audio/menu_b.ogg")
 end
 function beiCheeKiller_keypressed(key)
+    --set audio
+    beiCheeKiller.button:setVolume(getVol())
+    love.audio.rewind(beiCheeKiller.button)
+    beiCheeKiller.button:play()
     --select accept or cancel
     if beiCheeKiller.isSelect == true then
         if key == "left" and beiCheeKiller.stage == 2 then
+            beiCheeKiller.button:play()
             beiCheeKiller.stage = 1
         elseif key == "left" and beiCheeKiller.stage == 1 then
+            beiCheeKiller.button:play()
             beiCheeKiller.stage = 2
         elseif key == "right" and beiCheeKiller.stage == 2 then
+            beiCheeKiller.button:play()
             beiCheeKiller.stage = 1
         elseif key == "right" and beiCheeKiller.stage == 1 then
+            beiCheeKiller.button:play()
             beiCheeKiller.stage = 2
         end
     end
     --press space to select
     if key == " " and beiCheeKiller.stage == 1 then
+        beiCheeKiller.button:play()
         menu.isBCK = false
     elseif key == " " and beiCheeKiller.stage == 2  then
         --load save
-        local data = {}
-        local data2 = {}
-        local bool1, bool2
-        local f = love.filesystem.newFile("data.txt")
-        local f2 = love.filesystem.newFile("data2.txt")
-        f:open("r")
-        f2:open("r")
-        local count = 1
-        local reg = 0
-        for line in love.filesystem.lines("data.txt") do
-            if count % 2 == 1 then
-                reg = tonumber(line)
-            else
-                table.insert(data, reg, tonumber(line))
-            end
-            count = count + 1
-        end
-        for line in love.filesystem.lines("data2.txt") do
-            table.insert(data2, line)
-        end
-        f:close()
-        f2:close()
-        if data2[1] == "true" then
-            bool1 = true
-        else
-            bool1 = false
-        end
-        if data2[2] == "true" then
-            bool2 = true
-        else
-            bool2 = false
-        end
-        loveLoad({data, bool1, bool2, tonumber(data2[3]), tonumber(data2[4]), tonumber(data2[5])})
-        love_reloadDay()
+        beiCheeKiller.button:play()
+        save_load()
         menu.isBCK = false
         beiCheeKiller.stage = 1
         gameStage = 2
@@ -92,4 +71,40 @@ function beiCheeKiller_draw()
     elseif beiCheeKiller.stage == 2 and beiCheeKiller.isSelect == true then
         love.graphics.print("ACCEPT", beiCheeKiller.width/3 - beiCheeKiller.size2*3, beiCheeKiller.height/2 + 50)
     end
+end
+function save_load()
+    local data = {}
+    local data2 = {}
+    local bool1, bool2
+    local f = love.filesystem.newFile("data.txt")
+    local f2 = love.filesystem.newFile("data2.txt")
+    f:open("r")
+    f2:open("r")
+    local count = 1
+    local reg = 0
+    for line in love.filesystem.lines("data.txt") do
+        if count % 2 == 1 then
+            reg = tonumber(line)
+        else
+            table.insert(data, reg, tonumber(line))
+        end
+        count = count + 1
+    end
+    for line in love.filesystem.lines("data2.txt") do
+        table.insert(data2, line)
+    end
+    f:close()
+    f2:close()
+    if data2[1] == "true" then
+        bool1 = true
+    else
+        bool1 = false
+    end
+    if data2[2] == "true" then
+        bool2 = true
+    else
+        bool2 = false
+    end
+    loveLoad({data, bool1, bool2, tonumber(data2[3]), tonumber(data2[4]), tonumber(data2[5])})
+    love_reloadDay()
 end
