2a349a93a3e710a23fd609c8fcd07a53eaf37e39
Ya <wuandy1225@gmail.com>
2015-11-20 01:56:15 +0800

    improve world2.lua map

--- world.lua
+++ world.lua
@@ -441,10 +441,12 @@ function isBarrier(barrierX,barrierY)
         character.count=false
         world.count=false
     end
-    for i, monster in ipairs(monsters) do
-        if monster.nowX-world.x ==barrierX and monster.nowY-world.y == barrierY then
-            monster.nowX=monster.pastX
-            monster.nowY=monster.pastY
+    if day_state == 1 then
+        for i, monster in ipairs(monsters) do
+            if monster.nowX-world.x ==barrierX and monster.nowY-world.y == barrierY then
+                monster.nowX=monster.pastX
+                monster.nowY=monster.pastY
+            end
         end
     end
     for i=1,1 do
