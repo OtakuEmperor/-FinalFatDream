c86a3c755043754114b7642a1fba3980fa00727c
¡§sexy¡¨ <gt810034@gmail.com>
2015-11-28 22:31:22 +0800

    fix and add audio to ed.lua

--- ed.lua
+++ ed.lua
@@ -4,12 +4,15 @@ function ed_load()
     --get width and height of windows
     ed.width = love.graphics.getWidth()
     ed.height = love.graphics.getHeight()
+    --load audio
+    ed.bgm = love.audio.newSource("audio/ed.mp3")
     --set the end
     ed.theEnd = "xxTHE ENDxx"
     ed.size1 = 72
     ed.font1 = love.graphics.newFont("font/NotoSansCJKtc-Black.otf", ed.size1)
     ed.font3 = love.graphics.newFont("font/NotoSansCJKtc-Black.otf", 15)
     ed.range1 = 0
+    ed.opacity = 0
     --set subtitles
     local f = love.filesystem.newFile("ed.txt")
     f:open("r")
@@ -36,31 +39,29 @@ end
 
 function ed_draw()
     love.graphics.setBackgroundColor(0, 0, 0)
-
+    ed.bgm:setVolume(getVol()*0.8)
+    ed.bgm:play()
     --draw the end
-    love.graphics.setColor(255, 255, 255)
+    if ed.opacity < 255 then
+        love.graphics.setColor(255, 255, 255, ed.opacity)
+        ed.opacity = ed.opacity + 1
+    else
+        love.graphics.setColor(255, 255, 255, 255)
+    end
     love.graphics.setFont(ed.font1)
-    if ed.s < 4 then
+    if ed.s < 7 then
         love.graphics.print(ed.theEnd, ed.width/2-ed.size1*string.len(ed.theEnd)/3, ed.height/3)
-    elseif ed.s > 4 and (ed.height - ed.range1) > -72 then
+    elseif ed.s > 7 and (ed.height - ed.range1) > -72 then
         love.graphics.print(ed.theEnd, ed.width/2-ed.size1*string.len(ed.theEnd)/3, ed.height/3 - ed.range1)
         ed.range1 = ed.range1 + 1
     end
 
     --draw subtitle
-    if ed.s > 5 then
+    if ed.s > 7 then
         love.graphics.setFont(ed.font2)
         love.graphics.print(ed.text, ed.width/4, ed.height-ed.range2)
         ed.range2 = ed.range2 + 1
     end
-
-    --draw cover
-    if ed.s < 3 then
-        love.graphics.setColor(0, 0, 0, 255*(3 - ed.s)/3)
-    else
-        love.graphics.setColor(0, 0, 0, 0)
-    end
-    love.graphics.rectangle("fill", 0, 0, ed.width, ed.height)
 
     --draw s
     love.graphics.setColor(255,255,255)
